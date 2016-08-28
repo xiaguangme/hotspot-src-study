c492d1d8dceacd54aec3e527f00cfa2bfc9c091d
simon_S_ubu <chenxiaguang@gmail.com>
2016-08-28 23:18:12 +0800

    compile script

---
+++ hotspot/compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export LANG=C
+
+export ALT_BOOTDIR="/usr/jdk1.7.0_79"
+export ALT_JDK_IMPORT_PATH="/usr/jdk1.7.0_79"
+
+export ANT_HOME="/home/simomme/002.tool/apache-ant-1.9.4"
+
+export PATH="/usr/lib:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/simomme/002.tool/apache-ant-1.9.4:/usr/lib/x86_64-linux-gnu:/usr/lib/gcc/x86_64-linux-gnu/4.6"
+
+
+export HOTSPOT_BUILD_JOBS=5
+export ALT_OUTPUTDIR=../build/hotspot_debug
---
+++ hotspot/make/make.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+make jvmg jvmg1 2>&1|tee ../../build/hotspot_build_debug.log 
