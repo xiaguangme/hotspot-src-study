68bdd1f79c1ad92f1b8da94580d174d21826901b
simon_S_ubu <312399980@qq.com>
2016-12-04 13:52:15 +0800

    load class process and trace info

--- GDB常用调试命令.md
+++ GDB常用调试命令.md
@@ -3,9 +3,17 @@ s 进入方法内部
 n 下一行
 break java.c:1400  指定文件打断点
 
+条件断点
+set $loaderCounter = 0
+break classLoader.cpp:877  if ++$loaderCounter == 282
+
+
 
 高效调试GDB的贴子
 https://metricpanda.com/tips-for-productive-debugging-with-gdb
 可惜ubuntu12.04 自带的gdb版本过低（7.4） 用不了。  需要gdb版本>=7.7
 
-安装sudo apt-get install gdbserver
+
+
+
+
---
+++ hotspot/simon_test/src/EmptyClass.java
@@ -0,0 +1,25 @@
+/*
+ * 文 件 名:  EmptyClass.java
+ * 版    权:   . Copyright YYYY-YYYY,  All rights reserved
+ * 描    述:  <描述>
+ * 修 改 人:  simomme
+ * 修改时间:  2016-12-4
+ */
+
+/**
+ * <一句话功能简述>
+ * <功能详细描述>
+ * 
+ * @author  chenxiaguang@gmail.com
+ * @version  [版本号, 2016-12-4]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
+public class EmptyClass
+{
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
